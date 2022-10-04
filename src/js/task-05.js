7a03eaf87b5271957a77b08a0686866c57be8563
DariaSviatenko <svyatikdasha@gmail.com>
2022-10-04 15:11:54 +0300

    task5

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,6 +1,10 @@
-const textInput = document.querySelector(".text-input");
-const output = document.querySelector(".output");
 
-textInput.addEventListener("input", (event) => {
-  output.textContent = event.currentTarget.value;
-});
+const refs = {
+  textInput: document.querySelector('#name-input'),
+  output: document.querySelector('#name-output'),
+};
+
+refs.textInput.addEventListener('input', onInputChange);
+function onInputChange(event){
+  refs.output.textContent = event.currentTarget.value === "" ? "Anonymous" : event.currentTarget.value;
+}
---
+++ src/js/task-06.js
@@ -0,0 +1,31 @@
+// const textInput = document.querySelector(".text-input");
+// const setFocusBtn = document.querySelector('[data-action="set"]');
+// const removeFocusBtn = document.querySelector('[data-action="remove"]');
+
+// setFocusBtn.addEventListener("click", () => {
+//   textInput.focus();
+// });
+
+// removeFocusBtn.addEventListener("click", () => {
+//   textInput.blur();
+// });
+
+// textInput.addEventListener("focus", () => {
+//   textInput.value = "This input has focus";
+// });
+
+// textInput.addEventListener("blur", () => {
+//   textInput.value = "";
+// });
+{/* <body>
+    <p><a href="index.html">Go back</a></p>
+
+    <input
+      type="text"
+      id="validation-input"
+      data-length="6"
+      placeholder="Please enter 6 symbols"
+    />
+
+    <script src="js/task-06.js" type="module"></script>
+  </body> */}
