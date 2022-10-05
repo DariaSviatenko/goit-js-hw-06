5f711e9aff110637aacf17ba83fbc357449877c1
DariaSviatenko <svyatikdasha@gmail.com>
2022-10-05 17:42:56 +0300

    task9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
+
+const btn = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+btn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  span.textContent = randomColor;
+});
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
